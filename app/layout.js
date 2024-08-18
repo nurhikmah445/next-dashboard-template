ecd22dd293e2a4e68e55260999f36b76dd4e5658
nurulhikmahsam2004 <143022191+nurhikmah445@users.noreply.github.com>
2024-08-18 06:24:05 +0000

    selesai chapter3

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
 import "@/app/ui/global.css";
+import { lusitana } from '@/app/ui/fonts'; // Import the configured font
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
